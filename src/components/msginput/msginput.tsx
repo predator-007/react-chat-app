c3eeac77abcb3aa9572956a73da2daee48c3ccee
predator-007 <srijanraj2000@gmail.com>
2021-04-17 16:33:35 +0530

    commit

--- src/components/msginput/msginput.tsx
+++ src/components/msginput/msginput.tsx
@@ -8,24 +8,44 @@ const MsgInput:React.FC=()=>{
     const messageref=firestore.collection("messages");
     const [msg,setmsg]=useState<string>("");
     const user=useSelector((state:any)=>state.user);
+    const reciever=useSelector((state:any)=>state.reciever);
+    
+    function time(date:any) {
+    var d=date.getDate();
+    var m=date.getMonth()+1;
+    var year=date.getFullYear();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm + '  ' +d+'/'+m+'/'+year;
+    return strTime;
+    }
     const sendmsg=async(e:React.FormEvent)=>{
             e.preventDefault();
             await messageref.add({
                 text:msg,
                 createdAt:firebase.firestore.FieldValue.serverTimestamp(),
                 from:user.displayName,
+                to:reciever,
+                time:time(new Date),
             });
             setmsg("");
     }
 return(
     <div className="msginput">
-        <form onSubmit={(e)=>sendmsg(e)}>
-        <input type="text" className="form-contro" placeholder="Type a message..." 
+        <form onSubmit={(e)=>{
+            sendmsg(e);
+        }
+        }>
+        <input type="text" className="inputmessage" placeholder="Type a message..." 
         value={msg}
         onChange={(e)=>{setmsg(e.target.value)}}
         
         ></input>
-        <button type="submit" className="btn"><i  className="fas fa-camera"></i></button>
+        <button type="submit" className="btn"><i  style={{color:" rgb(47, 130, 224)"}}className="fa fa-paper-plane"></i></button>
         </form>
     </div>
 );
