01087702b00f79be800485be74a23c2161c12f29
predator-007 <srijanraj2000@gmail.com>
2021-04-17 13:48:21 +0530

    commit

--- src/containers/chatpage/index.tsx
+++ src/containers/chatpage/index.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import Chatarea from '../../components/chatarea';
 import Chatlog from '../../components/chatlog';
 import './style.css';
 const Chatpage:React.FC=()=>{
-
 return(
     <div className="chatpage">
         <Chatlog/>
         <Chatarea/>
     </div>
 );
-
 }
 export default Chatpage;
