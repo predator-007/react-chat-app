bb865c6e53e7686d982560f6489be26f54eae5c4
predator-007 <srijanraj2000@gmail.com>
2021-04-18 15:35:11 +0530

    commit

--- src/containers/login signup/login.tsx
+++ src/containers/login signup/login.tsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import { Button } from '@material-ui/core';
 import firebase from "firebase/app";
 import { useDispatch, useSelector, useStore } from 'react-redux';
 import { useraction } from '../../services/actions/user';
-
-import { Avatar, Grid, Paper, TextField } from '@material-ui/core';
 import './style.css';
-import { Button } from '@material-ui/core';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
-import { ClassTwoTone, CloudUpload, Facebook, MailOutline, Twitter } from '@material-ui/icons';
+import { Avatar, Grid, Paper, TextField } from '@material-ui/core';
+import { ChatBubbleOutline, ChatBubbleOutlined, Facebook, MailOutline, Twitter } from '@material-ui/icons';
 const Login : React.FC=()=>{
 
   /*
@@ -77,27 +76,34 @@ const Login : React.FC=()=>{
             adduser(result.user?.displayName);
           }).catch((err)=>console.log(err));
   }
+  const paperStyle={padding:20,height:'70vh',width:380,margin:"100px auto"};
+  const avatarStyle={backgroundColor:'#28c328'};
+  const b1Style={margin:"20px 40px"};
+  const b2Style={margin:"20px 35px"};
+  const unameStyle={margin:"8px 0"};
+  const headStyle={margin:"10px 0"};
+  const gmailStyle={margin:"5px 0"};
+  const fbStyle={margin:"5px 0"};
+  const twitterStyle={margin:"5px 0"};
    return(
-    <body className="bodylogin" >
-    <Grid>
-      <Paper elevation={10}>
-        <Grid container spacing ={0} justify='center' alignItems='center' direction='column'>
-          <Avatar ><ChatBubbleOutlineIcon/></Avatar>
-         <h2 >Sign In/Sign Up</h2>
-
-        </Grid>
-        <TextField label='Username' placeholder='Enter username' fullWidth required />
-        <TextField label='Password' placeholder='Enter password' type='password' fullWidth required/>
-        <Button variant='contained' type='submit' color='primary' >Sign In</Button>
-        <Button variant='contained' type='submit' color='primary' >Sign Up</Button>
-        <Button className="button"  variant="contained" color='primary' fullWidth startIcon={<MailOutline />}  onClick={()=>loginwithgoogle()}>Sign In Using Gmail</Button>
-        <Button className="button"  variant="contained" color='primary' fullWidth startIcon={<Facebook />}  onClick={()=>loginwithfacebook()}>Sign In Using Facebook</Button>
-        <Button className="button"  variant="contained" color='primary' fullWidth startIcon={<Twitter />}  onClick={()=>loginwithtwitter()}>Sign In Using Twitter</Button>
-      </Paper>
-    </Grid>
-    
-</body>
       
-);
-}
+      <body className="bodylogin" >
+        <Grid>
+          <Paper elevation={10} style={paperStyle}>
+            <Grid container spacing={0} justify='center' alignItems='center' direction='column'>
+              <Avatar style={avatarStyle}><ChatBubbleOutlineIcon/></Avatar>
+            <h2 style={headStyle}>Sign In/Sign Up</h2>
+            </Grid>
+            <TextField label='Username' placeholder='Enter Username' fullWidth required style={unameStyle}/>
+            <TextField label='Password' placeholder='Enter Password' fullWidth required/>
+            <Button variant='contained' type='submit' color='primary' style={b1Style}>Sign In</Button>
+            <Button variant='contained' type='submit' color='primary' style={b2Style}>Sign Up</Button>
+            <Button type='submit' style={gmailStyle} variant="contained" color='primary' fullWidth startIcon={<MailOutline/>} onClick={()=>loginwithgoogle()}>Sign In using Gmail</Button>
+            <Button type='submit' style={fbStyle} variant="contained" color='primary' fullWidth startIcon={<Facebook/>} onClick={()=>loginwithfacebook()}>Sign In using Facebook</Button>
+            <Button type='submit' style={twitterStyle} variant="contained" color='primary' fullWidth startIcon={<Twitter/>} onClick={()=>loginwithtwitter()}>Sign In using Twitter</Button>
+          </Paper>
+        </Grid>
+    </body>
+   );
+};
 export default Login;
