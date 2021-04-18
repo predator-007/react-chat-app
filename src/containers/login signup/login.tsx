8bf8fb6cbec9b61f8a2761db478da657440dd1b5
mdpasha0218 <sameerpsh15@gmail.com>
2021-04-18 12:37:05 +0530

    Src changed

--- src/containers/login signup/login.tsx
+++ src/containers/login signup/login.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Button } from "react-bootstrap";
+import { Button } from '@material-ui/core';
 import firebase from "firebase/app";
 import { useDispatch, useSelector, useStore } from 'react-redux';
 import { useraction } from '../../services/actions/user';
 import './style.css';
+import { Avatar, Grid, Paper, TextField } from '@material-ui/core';
+import { ChatBubbleOutline, ChatBubbleOutlined, Facebook, MailOutline, Twitter } from '@material-ui/icons';
 const Login : React.FC=()=>{
 
   /*
@@ -56,10 +58,33 @@ const Login : React.FC=()=>{
             adduser(result.user?.displayName);
           }).catch((err)=>console.log(err));
   }
+  const paperStyle={padding:20,height:'70vh',width:380,margin:"100px auto"};
+  const avatarStyle={backgroundColor:'#28c328'};
+  const b1Style={margin:"20px 40px"};
+  const b2Style={margin:"20px 35px"};
+  const unameStyle={margin:"8px 0"};
+  const headStyle={margin:"10px 0"};
+  const gmailStyle={margin:"5px 0"};
+  const fbStyle={margin:"5px 0"};
+  const twitterStyle={margin:"5px 0"};
    return(
       <body className="bodylogin" >
-        <Button className="button" variant="danger" onClick={()=>login()}>login</Button>
+        <Grid>
+          <Paper elevation={10} style={paperStyle}>
+            <Grid container spacing={0} justify='center' alignItems='center' direction='column'>
+              <Avatar style={avatarStyle}><ChatBubbleOutline/></Avatar>
+            <h2 style={headStyle}>Sign In/Sign Up</h2>
+            </Grid>
+            <TextField label='Username' placeholder='Enter Username' fullWidth required style={unameStyle}/>
+            <TextField label='Password' placeholder='Enter Password' fullWidth required/>
+            <Button variant='contained' type='submit' color='primary' style={b1Style}>Sign In</Button>
+            <Button variant='contained' type='submit' color='primary' style={b2Style}>Sign Up</Button>
+            <Button type='submit' style={gmailStyle} variant="contained" color='primary' fullWidth startIcon={<MailOutline/>} onClick={()=>login()}>Sign In using Gmail</Button>
+            <Button type='submit' style={fbStyle} variant="contained" color='primary' fullWidth startIcon={<Facebook/>} onClick={()=>login()}>Sign In using Facebook</Button>
+            <Button type='submit' style={twitterStyle} variant="contained" color='primary' fullWidth startIcon={<Twitter/>} onClick={()=>login()}>Sign In using Twitter</Button>
+          </Paper>
+        </Grid>
     </body>
 );
-}
+};
 export default Login;
